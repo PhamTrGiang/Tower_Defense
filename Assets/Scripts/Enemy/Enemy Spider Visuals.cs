b62205688914bb120cdde64372b2b992f45300ce
PhamTrGiang <ptg3012@gmail.com>
2025-07-23 15:55:20 +0700

    Animation Leg Spider Boss

--- Assets/Scripts/Enemy/Enemy Spider Visuals.cs
+++ Assets/Scripts/Enemy/Enemy Spider Visuals.cs
@@ -2,5 +2,29 @@ using UnityEngine;
 
 public class EnemySpiderVisuals : EnemyVisuals
 {
-    
+    [Header("Leg Details")]
+    public float legSpeed = 3;
+
+    private SpiderLeg[] legs;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        legs = GetComponentsInChildren<SpiderLeg>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        UpdateSpiderLeg();
+    }
+
+    private void UpdateSpiderLeg()
+    {
+        foreach (var leg in legs)
+        {
+            leg.UpdateLeg(legSpeed);
+        }
+    }
 }
---
+++ Assets/Scripts/Enemy/Spider Leg.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpiderLeg : MonoBehaviour
+{
+    [SerializeField] private float moveThreshold = .45f;
+    private bool shouldMove;
+    private bool canMove = true;
+    private Coroutine moveCo;
+
+    [Header("Leg Setup")]
+    [SerializeField] private SpiderLeg oppositeLeg;
+    [SerializeField] private SpiderLegReference legRef;
+    [SerializeField] private Transform actualTarget;
+    [SerializeField] private Transform bottomLeg;
+    [SerializeField] private Vector3 placementOffset;
+    private Transform worldTargetReference;
+
+    private void Awake()
+    {
+        worldTargetReference = Instantiate(new GameObject("Ref_Point"), actualTarget.position, Quaternion.identity).transform;
+        worldTargetReference.gameObject.name = legRef.gameObject.name + "_world";
+    }
+
+    public void UpdateLeg(float legSpeed)
+    {
+        actualTarget.position = worldTargetReference.position + placementOffset;
+        shouldMove = Vector3.Distance(worldTargetReference.position, legRef.ContactPoint()) > moveThreshold;
+
+        if (bottomLeg != null)
+            bottomLeg.forward = Vector3.down;
+
+        if (shouldMove && canMove)
+        {
+            if (moveCo != null)
+                StopCoroutine(moveCo);
+            moveCo = StartCoroutine(LegMoveCo(legSpeed));
+        }
+    }
+
+    private IEnumerator LegMoveCo(float legSpeed)
+    {
+        oppositeLeg.CanMove(false);
+
+        while (Vector3.Distance(worldTargetReference.position, legRef.ContactPoint()) > .01f)
+        {
+            worldTargetReference.position = Vector3.MoveTowards(worldTargetReference.position, legRef.ContactPoint(), legSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        oppositeLeg.CanMove(true);
+    }
+
+    public void CanMove(bool enableMovement) => canMove = enableMovement;
+}
